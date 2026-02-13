4e10e40e4d519d154055cb585459d5cd14546aa3
Madeeha <fathimamadeeha.m@gmail.com>
2026-02-13 15:58:01 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,13 @@
+public class PalindromeCheckerApp {
+    /**
+     * Application entry point
+     *
+     * this is a java Palindrome checker App Welcome message
+     *
+     * @author Developer
+     * @version 1.0
+     */
+    public static void main(String[] args){
+    System.out.println("Welcome to the Palindrome Checker Management System\n" +"Version : 1.0\n" +
+            "System initialized successfully.");}
+}
