b35f6fcc70828d70c1843b19aacda99ea58ee79d
harpreetsingh79005 <harpreet79005@gmail.com>
2026-03-09 16:01:22 +0530

    UC2

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,14 +1,21 @@
-public class PalindromeCheckerApp {
-    /**
-     * Application entry point
-     *
-     * this is a java Palindrome checker App Welcome message
-     *
-     * @author Developer
-     * @version 1.0
-     */
-    public static void main(String[] args){
-        System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("System initialized successfully.");
+public class UseCase2PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String input = "madam";
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println(input + " is a Palindrome.");
+        } else {
+            System.out.println(input + " is NOT a Palindrome.");
+        }
     }
 }
